f36099f18b5b91da1a29d56eeb629ad1c2effc37
Karin Aizawa <kaizawa77@gmail.com>
2020-10-05 16:23:29 -0700

    make sure to run npm run build again

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 // const cors = require('cors');
+// app.use(cors());
+
 const port = process.env.PORT || 9999;
 
-// app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
