18f886f5b79e10b3a553fe4839a4a4c3f182dd0f
Karin Aizawa <kaizawa77@gmail.com>
2020-10-05 17:07:12 -0700

    more changes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
 const app = express();
-
-// const cors = require('cors');
-// app.use(cors());
-
+app.use(cors());
 const port = process.env.PORT || 9999;
 
-app.use(express.static(path.join(__dirname, '../public')));
+
+const proxy = createProxyMiddleware({
+  target: 'http://localhost:7777',
+  changeOrigin: true, // for vhosted sites, changes host header to target's host
+  logLevel: 'debug',
+});
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/review/:productId', proxy);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}!`)
-})
+  console.log(`Proxy running on port ${port}.`);
+});
