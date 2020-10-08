3232ef458dfa554362eefc9d1fd7b6b8a1697b7e
Karin Aizawa <kaizawa77@gmail.com>
2020-10-07 17:57:51 -0700

    html, server code

--- server/index.js
+++ server/index.js
@@ -1,23 +1,56 @@
 const express = require('express');
 const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
+const port = 9999;
+
+// app.use(express.static(`${__dirname}/../client/dist`));
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(cors());
-const port = process.env.PORT || 9999;
 
+app.get('/products/:src/:id', (req, res) => {
+  const { src, id } = req.params;
+  let path;
 
-const proxy = createProxyMiddleware({
-  target: 'http://localhost:7777',
-  changeOrigin: true, // for vhosted sites, changes host header to target's host
-  logLevel: 'debug',
+  if(src === 'featureExplorer') {
+    path = 'http://localhost:3000';
+  }
+  else if(src === 'relatedProducts') {
+    path = 'http://localhost:3001';
+  }
+  else if(src === 'productInfo') {
+    path = 'http://localhost:3002';
+  }
+
+  axios.get(`${path}/products/${id}`)
+  .then((response) => {
+    res.status(200).send(response.data);
+  })
+  .catch((error) => {
+    res.status(400).send(error);
+  })
 });
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/review/:productId', proxy);
+const Proxy = (targetUrl) => (req, res) => {
+  axios.get(targetUrl + req.originalUrl + req.params.id)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+};
+const proxyQuestions = Proxy('http://localhost:3004');
+const proxyReviews = Proxy('http://localhost:7777');
+const proxyRelatedProducts = Proxy('http://localhost:3001');
+
+app.use('/questions/:id', proxyQuestions);
+app.use('/review/:id', proxyReviews);
+app.use('/relatedproducts/:id', proxyRelatedProducts);
 
 app.listen(port, () => {
-  console.log(`Proxy running on port ${port}.`);
+  console.log(`listening on port ${port}`);
 });
